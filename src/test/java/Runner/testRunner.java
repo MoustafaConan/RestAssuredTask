b3c7c1fe588a0e374473abd4117e43a3867775c2
MostafaConan <me@mostafamansour.com>
2021-11-14 02:28:43 +0200

    refactoring

--- src/test/java/Runner/testRunner.java
+++ src/test/java/Runner/testRunner.java
@@ -4,8 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src\\test\\java\\Features\\", glue = {"Steps"}, plugin = {"pretty", "html:target/cucumber-reports.html"},
-        monochrome = true
-)
+        monochrome = true)
 public class testRunner extends AbstractTestNGCucumberTests {
 
 }
