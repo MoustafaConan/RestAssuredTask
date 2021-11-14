3ddc4ed6d52a82981b8b975bd823222cbe515626
MostafaConan <me@mostafamansour.com>
2021-11-14 02:15:59 +0200

    remove unused imports

--- src/test/java/Steps/BaseSteps.java
+++ src/test/java/Steps/BaseSteps.java
@@ -1,11 +1,9 @@
 package Steps;
 
-import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.restassured.response.Response;
 
 public class BaseSteps {
     Response response;
     Scenario scenario;
-	//To elimante duplication
 }
